5bd461e920ddbada16fc9580d85569b2e9685cbb
Fabrizio <Fabrizio_39@hotmail.com>
2018-09-24 20:22:18 +0200

    Updates

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// Vanilla JS 
+// logic is responsible to hide/display the menu
+
+// Constants
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set initial state of menu
+let showMenu = false;
+
+// Add listener on menu button
+menuBtn.addEventListener('click', toggleMenu);
+
+// Functions
+function toggleMenu() {
+    // Menu is closed
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        // set menu state
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        // set menu state
+        showMenu = false;
+    }
+}
